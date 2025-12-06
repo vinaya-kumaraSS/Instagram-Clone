0e474da4b4e7f1c31a8365a41355023c5840ee82
vinaya-kumaraSS <vinaykumarss904@gmail.com>
2025-12-06 14:49:09 +0530

    added

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: ,
+      home: (),
     );
   }
 }
