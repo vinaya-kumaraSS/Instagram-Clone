b62726d2fb5ed462a7134e38a6fb6f0525b0013f
vinaya-kumaraSS <vinaykumarss904@gmail.com>
2025-12-13 14:03:11 +0530

    textfield added

--- lib/screens/authScreens/forGotPasswordScreen.dart
+++ lib/screens/authScreens/forGotPasswordScreen.dart
@@ -1,24 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/widgets/uiHelper.dart';
 
 class Forgotpasswordscreen extends StatelessWidget {
   const Forgotpasswordscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController emailOrUsernameController = TextEditingController();
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
-        padding: const EdgeInsets.all(12.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               "Find yout account",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
             ),
             SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.only(left: 2),
-              child: Text("Enter your email or username"),
+            Text("Enter your email or username."),
+            SizedBox(height: 5),
+            Uihelper.hyperLinkText(
+              text: "Can't reset your password?",
+              fontSize: 12,
+              fontWeight: FontWeight.normal,
+              fontColor: Color(0XFF3797EF),
+              onTap: () {},
+            ),
+            SizedBox(height: 50),
+            TextField(
+              controller: emailOrUsernameController,
+              obscureText: false,
+              cursorColor: Color(0xFFFFFFFF),
+              decoration: InputDecoration(
+                hintText: "Email or username",
+                labelText: "Email or username",
+                hintStyle: TextStyle(
+                  fontSize: 14,
+                  color: Color(0xFFFFFFFF),
+                  fontFamily: 'regular',
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  borderSide: BorderSide(color: Color.fromARGB(6, 48, 48, 48), width: 1),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  borderSide: BorderSide(color: Color(0xFFFFFFFF), width: 1),
+                ),
+              ),
             ),
           ],
         ),
