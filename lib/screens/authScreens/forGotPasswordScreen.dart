e2fb1654e82e19f5cbc8783e43d1d7f883586774
vinaya-kumaraSS <vinaykumarss904@gmail.com>
2025-12-13 13:33:46 +0530

    added

--- lib/screens/authScreens/forGotPasswordScreen.dart
+++ lib/screens/authScreens/forGotPasswordScreen.dart
@@ -17,7 +17,7 @@ class Forgotpasswordscreen extends StatelessWidget {
             ),
             SizedBox(height: 10),
             Padding(
-              padding: const EdgeInsets.only(left: 4),
+              padding: const EdgeInsets.only(left: 2),
               child: Text("Enter your email or username."),
             ),
           ],
